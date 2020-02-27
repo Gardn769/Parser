5f3d686823d9b979e3c68978fe13477c73aac914
Gardn <gardn769@gmail.com>
2020-02-27 21:01:00 +0300

    комичу в мастер на верочку

--- GG.js
+++ GG.js
@@ -1,2 +1,8 @@
-console.log("test");
-console.log(123);
+const fs = require('fs');
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const request = require('request');
+const pdf = require('pdf-parse');
+const needle = require('needle');
+//БД
+const Sequelize = require("sequelize");
