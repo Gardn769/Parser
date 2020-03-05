7f333ff40f640c7c9a198776cff1867b0957d770
Никита <nik.oschepkov@yandex.ru>
2020-03-05 22:18:20 +0300

    qwerty

--- GG.js
+++ GG.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
-const jsdom = require("jsdom");
+const jsdo0m = require("jsdom");
 const { JSDOM } = jsdom;
 const request = require('request');
 const phf = require('pdf-parse');
 const needle = require('needle');
-
+if (1) 0;
 const Sequelize = require("sequelize");
 
